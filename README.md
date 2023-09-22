a67c0f2626c849a97660e06cfb1513b695da6646
Bacilio Alexander Bolanos <babolanos@uvg.edu.gt>
2023-09-22 12:52:07 -0600

    feat: Added Winston

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # adminSys2023
+
 Administración de Sistemas 2023 - Universidad del Valle de Guatemala
+
+## Important Scripts:
+
+-   npm run dev: Starts a new nodemon instance for live development.
+-   npm run test: Runs jest
--- backend/routers/external.js
+++ backend/routers/external.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const { getPersonsController } = require('../controllers/persons');
 const router = express.Router();
+const logger = require('../config/logger');
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
-    console.log(
-        '# request: ',
-        JSON.stringify({
+    logger.log({
+        level: 'info',
+        message: JSON.stringify({
             when: Date.now(),
             url: req.url,
             params: req.params,
             body: req.body,
             method: req.method,
-        })
-    );
+        }),
+    });
+
     next();
 });
 
