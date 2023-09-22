1101ab9ec4f7dc436eafa3db379929b60669e838
Bacilio Alexander Bolanos <babolanos@uvg.edu.gt>
2023-09-22 13:35:33 -0600

    fix:US2-backend: Corrected api endpoint, created logger middleware

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,24 @@
 'use strict';
-
 const express = require('express');
+const persons = require('./routers/external');
+const logger = require('./middlewares/logger');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
+const PORT = process.env.PORT || 3002;
+const HOST = process.env.HOST || '0.0.0.0';
 
 // MIDDLEWARES:
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(logger);
 
-app.get("/version", (req, res) => {
-    return res.status(200).send("version: 1.0.0, date:"+Date.now());
+// ROUTERS:
+app.use('/api', persons);
+
+app.get('/version', (req, res) => {
+    return res.status(200).send('version: 1.0.0, date:' + Date.now());
 });
 
-app.listen(PORT, HOST, ()=>{
+app.listen(PORT, HOST, () => {
     console.log(`Server is up and running on http://${HOST}:${PORT}`);
 });
--- backend/routers/external.js
+++ backend/routers/external.js
@@ -1,23 +1,6 @@
 const express = require('express');
 const { getPersonsController } = require('../controllers/persons');
 const router = express.Router();
-const logger = require('../config/logger');
-
-// middleware that is specific to this router
-router.use((req, res, next) => {
-    logger.log({
-        level: 'info',
-        message: JSON.stringify({
-            when: Date.now(),
-            url: req.url,
-            params: req.params,
-            body: req.body,
-            method: req.method,
-        }),
-    });
-
-    next();
-});
 
 router.get('/personas/:total', getPersonsController);
 
