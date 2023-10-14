bf7d63ab5bf63daac9a4d77ef9e1384734a76a18
Bacilio Alexander Bolanos <babolanos@uvg.edu.gt>
2023-10-13 22:07:59 -0600

    backend: corrected cors issue

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@
 const express = require('express');
 const persons = require('./routers/external');
 const logger = require('./middlewares/logger');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
 // MIDDLEWARES:
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger);
